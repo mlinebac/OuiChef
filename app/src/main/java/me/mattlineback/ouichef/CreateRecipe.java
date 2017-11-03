bbbe7efdac11f9b1516b5cd2b9fce493b160d38e
mlinebac <mlinebac@unca.edu>
2017-11-03 09:19:32 -0400

    added recipe menu screen

--- app/src/main/java/me/mattlineback/ouichef/CreateRecipe.java
+++ app/src/main/java/me/mattlineback/ouichef/CreateRecipe.java
@@ -1,13 +1,32 @@
 package me.mattlineback.ouichef;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class CreateRecipe extends AppCompatActivity {
-
+    @BindView(R2.id.button_home)
+    Button home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recipe);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R2.id.button_home)
+    public void submit(View view) {
+        if (view == home) {
+            Intent intent = new Intent(CreateRecipe.this, HomeScreen.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
---
+++ app/src/main/java/me/mattlineback/ouichef/RecipeMenu.java
@@ -0,0 +1,50 @@
+package me.mattlineback.ouichef;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class RecipeMenu extends AppCompatActivity {
+
+    @BindView(R2.id.button_search)
+    Button btnSearch;
+    @BindView(R2.id.button_create)
+    Button btnCreate;
+    @BindView(R2.id.button_upload)
+    Button btnUpload;
+    @BindView(R2.id.button_home)
+    Button btnHome;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_menu);
+        ButterKnife.bind(this);
+    }
+    public void getIntent(Class c){
+        Intent intent = new Intent(RecipeMenu.this, c);
+        startActivity(intent);
+        finish();
+    }
+
+    @OnClick({R2.id.button_search, R2.id.button_create, R2.id.button_upload, R2.id.button_home})
+    public void submit(View view) {
+        if (view == btnSearch) {
+            getIntent(RecipeDisplay.class);
+        }else if (view == btnCreate){
+            getIntent(CreateRecipe.class);
+        }else if(view == btnUpload){
+            getIntent(RecipeList.class);
+        }else{
+            getIntent(HomeScreen.class);
+        }
+    }
+
+}
