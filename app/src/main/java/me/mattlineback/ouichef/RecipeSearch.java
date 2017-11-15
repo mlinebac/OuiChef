59cd13a336e05e5de8337bc4c2cb9bde2a19be8b
mlinebac <mlinebac@unca.edu>
2017-11-15 00:22:56 -0500

    added recipe adapter to RecipeSearch class

--- app/src/main/java/me/mattlineback/ouichef/RecipeSearch.java
+++ app/src/main/java/me/mattlineback/ouichef/RecipeSearch.java
@@ -1,33 +1,34 @@
 package me.mattlineback.ouichef;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.firebase.ui.database.FirebaseListAdapter;
-import com.firebase.ui.database.FirebaseListOptions;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-import static android.webkit.ConsoleMessage.MessageLevel.LOG;
-
 public class RecipeSearch extends AppCompatActivity {
-    private final String TAG = "recipeDisplay";
+    private final String TAG = "RecipeSearch";
 
     DatabaseReference myRef;
     Query query;
+    Query querySearch;
     @BindView(R2.id.button_home)
     Button home;
     @BindView(R2.id.double_button)
@@ -37,62 +38,87 @@ public class RecipeSearch extends AppCompatActivity {
     @BindView(R2.id.name_recipes)
     EditText searchRecipe;
     @BindView(R2.id.recipe_view)
-    ListView recipeView;
+    RecyclerView recipeView;
 
-    FirebaseListAdapter<RecipeItem> adapter;
-    String recipe = "jam";
+    ArrayList<RecipeItem> recipeList;
+    RecipesAdapter adapter;
+    String recipe = " ";
+    LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_search);
         ButterKnife.bind(this);
-
+        recipeList = new ArrayList<>();
+        linearLayoutManager = new LinearLayoutManager(this);
+        recipeView.setLayoutManager(linearLayoutManager);
+        adapter = new RecipesAdapter(this, recipeList);
+        recipeView.setAdapter(adapter);
         this.myRef = FirebaseDatabase.getInstance().getReference("recipes");
-        query = myRef.child(recipe);
+        query = myRef.orderByKey();
+        querySearch = myRef.child("recipeSearch");
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 recipe = searchRecipe.getText().toString();
-                query = myRef.child(recipe);
-                adapter.startListening();
+                searchRecipe.setText("");
+                myRef.child("recipeSearched").setValue(recipe);
+                recipeList.clear();
+
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                if (dataSnapshot.getKey().equals(recipe)) {
+                    getAllItems(dataSnapshot);
+
+                    }
+
+                }
+
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                if(dataSnapshot.getKey().equals(recipe)){
+                    for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        RecipeItem changedItem = snapshot.getValue(RecipeItem.class);
+                        recipeList.add(changedItem);
+                        adapter.notifyDataSetChanged();
+                    }
+
+                }
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
-    }
-    @Override
-    public void onStart(){
-        super.onStart();
-
-        FirebaseListOptions<RecipeItem> options = new FirebaseListOptions.Builder<RecipeItem>()
-                .setLayout(android.R.layout.activity_list_item)
-                .setQuery(query, RecipeItem.class)
-                .build();
-
-        adapter = new FirebaseListAdapter<RecipeItem>(options) {
-            @Override
-            protected void populateView(View v, RecipeItem model, int position) {
-                TextView listRecipe = v.findViewById(android.R.id.text1);
-                Log.d(TAG, "populate: " + listRecipe );
-                listRecipe.setTextColor(Color.WHITE);
-                String str = model.getRecipeItem();
-                (listRecipe).setText(str);
             }
-
-        };
-
-        recipeView.setAdapter(adapter);
-
-    }
-    @Override
-    public void onStop(){
-        super.onStop();
-        if(adapter != null){
-            adapter.stopListening();
+        });
+    }//onCreate
+    private void getAllItems(DataSnapshot dataSnapshot) {
+        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+            RecipeItem item = snapshot.getValue(RecipeItem.class);
+            recipeList.add(item);
+            Log.d(TAG, "recipeItem Added" + item);
         }
+        adapter.notifyDataSetChanged();
+        adapter = new RecipesAdapter(RecipeSearch.this, recipeList);
+        recipeView.setAdapter(adapter);
     }
-
     @OnClick(R2.id.button_home)
     public void submit(View view) {
         if (view == home) {
